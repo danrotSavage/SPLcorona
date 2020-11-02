b7956e048f61f0664b76fd02d1c32e5ec1901e7a
danrotSavage <danrot@post.bgu.ac.il>
2020-11-02 18:33:55 +0200

    starting new project, added Graph.cpp

---
+++ Graph.cpp
@@ -0,0 +1,26 @@
+//
+// Created by spl211 on 02/11/2020.
+//
+
+using namespace std;
+#include "Graph.h"
+Graph::Graph(std::vector<std::vector<int>> matrix):edges(matrix){
+
+    trafficLight = new vector<int>;
+    for (int i = 0; i < matrix.size(); ++i) {
+        trafficLight->push_back(0);
+    }
+
+
+
+}
+
+void Graph::infectNode(int nodeInd) {
+    if ((*trafficLight)[nodeInd] == 0)
+        (*trafficLight)[nodeInd] = 1;
+
+}
+bool Graph::isInfected(int nodeInd) {
+
+    return (*trafficLight)[nodeInd]==2;
+}
---
+++ Tree.cpp
@@ -0,0 +1,7 @@
+//
+// Created by spl211 on 01/11/2020.
+//
+
+#include "Tree.h"
+
+Tree*
